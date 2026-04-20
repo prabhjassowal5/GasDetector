a42f9af5375dc728941221c5b472806200c3f60d
Prabsimran singh <prabhjassowal5@gmail.com>
2026-04-20 11:16:02 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+type="module"
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAdElw2eCfm2tI5SiplNLixrqileFwQ_3o",
+    authDomain: "gasdetector-bba6a.firebaseapp.com",
+    projectId: "gasdetector-bba6a",
+    storageBucket: "gasdetector-bba6a.firebasestorage.app",
+    messagingSenderId: "658011014751",
+    appId: "1:658011014751:web:1370b32acec3a23207bdea"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
